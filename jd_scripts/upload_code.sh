7d7c5c1875a10904f3edc88b4c1d86f6f64e677c
Billy Yang <mz.yyam@gmail.com>
2020-11-20 10:03:28 +0800

    upload code sh

---
+++ jd_scripts/upload_code.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# ilaipi and laopo codes
+wget -q --delete-after http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDUyMjEwMDAwMDAwMzkzNjgxNjU=/
+wget -q --delete-after http://api.turinglabs.net/api/v1/jd/pet/create/MTAxODExNDYxMTEwMDAwMDAwMzkyNjE0Mzc=/
+
+wget -q --delete-after http://api.turinglabs.net/api/v1/jd/bean/create/e7lhibzb3zek2hlx7lig7rsenvltfqcyywpkyhi/
+wget -q --delete-after http://api.turinglabs.net/api/v1/jd/bean/create/cgzxa7pvaduxkhfjybow3kvlxi/
+
+wget -q --delete-after http://api.turinglabs.net/api/v1/jd/farm/create/208ebf2df56f47018e3abc6638ff6c16/
+wget -q --delete-after http://api.turinglabs.net/api/v1/jd/farm/create/085b2dee3e4c4d92877f1aba1e84da2d/
